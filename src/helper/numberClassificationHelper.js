d26f8f9d2ddcd65f9aeb0c48528ee8e9ea3d7533
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-02-02 23:32:46 +0000

    deployement

--- src/helper/numberClassificationHelper.js
+++ src/helper/numberClassificationHelper.js
@@ -1,4 +1,4 @@
-exports.isPrime = (num) => {
+exports.isPrime = (n) => {
   if (n < 2) return false;
   for (let i = 2; i * i <= n; i++) {
     if (n % i === 0) return false;
@@ -6,7 +6,7 @@ exports.isPrime = (num) => {
   return true;
 };
 
-exports.isPerfect = (num) => {
+exports.isPerfect = (n) => {
   if (n <= 1) return false;
 
   let sum = 1;
