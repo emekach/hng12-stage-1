42e00fc9c3faa8443c0aa8cce728311bf303fad4
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-02-02 21:19:54 +0000

    chore: add index route for deployment

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const compression = require('compression');
 const cors = require('cors');
-const  globalError  = require('./utils/globalError');
-const  limiter  = require('./utils/rateLimiter');
+const globalError = require('./utils/globalError');
+const limiter = require('./utils/rateLimiter');
 const classifyNumber = require('./routes/classifyNumber');
 
 const app = express();
@@ -13,6 +13,10 @@ app.use(limiter);
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '15kb' }));
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 
 app.use('/api/classify-number', classifyNumber);
 app.all('*', (req, res) => {
