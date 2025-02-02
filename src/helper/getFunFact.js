272fe324bc1dec8906553d4e06345da5dfdac850
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-02-02 22:24:20 +0000

    deployement

--- src/helper/getFunFact.js
+++ src/helper/getFunFact.js
@@ -1,10 +1,15 @@
 const { default: axios } = require('axios');
-
+const { isArmstrong } = require('./../helper/numberClassificationHelper');
 exports.getFunFact = async (num) => {
+  if (isArmstrong(num)) {
+    const digits = String(num).split('');
+    const explanation = digits.map((d) => `${d}^${digits.length}`).join(' + ');
+    return `${num} is an Armstrong number because ${explanation} = ${num}.`;
+  }
   try {
     const response = await axios.get(`http://numbersapi.com/${num}/math`);
     return response.data;
-  } catch (err) {
-    return 'No fun fact available for this number.';
+  } catch (error) {
+    return 'No fun fact available.';
   }
 };
