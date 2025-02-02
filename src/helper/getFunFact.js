a88f26d20987cbd5effed50d1d6acc6d5d3caa11
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-02-02 21:57:16 +0000

    deployement

--- src/helper/getFunFact.js
+++ src/helper/getFunFact.js
@@ -3,7 +3,7 @@ const { default: axios } = require('axios');
 exports.getFunFact = async (num) => {
   try {
     const response = await axios.get(`http://numbersapi.com/${num}/math?json`);
-    return response.data;
+    return response.data.text;
   } catch (err) {
     return 'No fun fact available for this number.';
   }
