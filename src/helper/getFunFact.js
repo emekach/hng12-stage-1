5606b3200afcd5704bcab9b9f54893a8073a8e95
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-02-02 22:02:48 +0000

    deployement

--- src/helper/getFunFact.js
+++ src/helper/getFunFact.js
@@ -3,7 +3,22 @@ const { default: axios } = require('axios');
 exports.getFunFact = async (num) => {
   try {
     const response = await axios.get(`http://numbersapi.com/${num}/math?json`);
-    return response.data.text;
+    let funFact = response.data.text;
+
+    funFact = funFact.replace(/narcissistic number/i, 'Armstrong number');
+
+    if (funFact.includes('Armstrong number')) {
+      const digits = num.toString().split('');
+      const sum = digits.reduce(
+        (acc, digit) => acc + Math.pow(parseInt(digit), digits.length),
+        0
+      );
+      funFact += ` because ${digits
+        .map((d) => `${d}^${digits.length}`)
+        .join(' + ')} = ${num}`;
+    }
+
+    return funFact;
   } catch (err) {
     return 'No fun fact available for this number.';
   }
